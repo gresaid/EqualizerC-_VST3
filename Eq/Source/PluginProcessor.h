536184acc2e5304b39c521eb52db9dfa6006c18c
gres.aid <gresvladimir02@gmail.com>
2021-12-01 19:12:37 +0600

    Установка и настройка dcp

--- Eq/Source/PluginProcessor.h
+++ Eq/Source/PluginProcessor.h
@@ -59,6 +59,12 @@ public:
     };
 
 private:
+
+    using Filter = juce::dsp::IIR::Filter<float>;
+    using CutFilter = juce::dsp::ProcessorChain < Filter, Filter, Filter, Filter>;
+    using MonoChain = juce::dsp::ProcessorChain <CutFilter, Filter, CutFilter>;
+
+    MonoChain leftChain, rigthChain;
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EqAudioProcessor)
 };
